913d3a81776d3a3914631dd089ffac21d633271b
JJWheel <josiah.wheeler@outlook.com>
2026-08-28 13:22:15 -0500

    new files

--- chapter 2/archimedes.py
+++ chapter 2/archimedes.py
@@ -1,10 +1,15 @@
 # make a spiral
 import turtle as t
+import math
+# defining variables
+t.speed(0)
+a = .5
+theta = 0
 
-t.begin_fill()
-for i in range(200):
-    t.forward(20)
-    t.left(45)
-t.end_fill()
-
-t.done()
+for i in range(3600):
+    r = a * theta
+    x = r * math.cos(theta)
+    y = r * math.sin(theta)
+    t.goto(x, y)
+    theta += 0.05
+t.done
