3822174b4ab774ee09d58016dd4bff373f0c3d4b
JJWheel <josiah.wheeler@outlook.com>
2026-08-28 13:07:52 -0500

    Finished spirals.py

---
+++ chapter 2/archimedes.py
@@ -0,0 +1,10 @@
+# make a spiral
+import turtle as t
+
+t.begin_fill()
+for i in range(200):
+    t.forward(20)
+    t.left(45)
+t.end_fill()
+
+t.done()
--- chapter 2/spirals.py
+++ chapter 2/spirals.py
@@ -3,6 +3,22 @@
 import turtle as t
 import math
 
+# turtle config
+t.bgcolor('black')
+t.color('white')
+t.shape('turtle')
+t.pensize(3)
+
 # defining variables
-a = 1
-b = 1
+a = 0.5 # growth factor
+b = 0.3 # growth rate
+theta = 5 # starting angle
+
+# draw
+for i in range(360):
+    r = a * math.exp(b * theta)
+    x = r * math.cos(theta)
+    y = r * math.sin(theta)
+    t.goto(x, y)
+    theta += 0.1
+t.done
