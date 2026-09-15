9fa8d5f1864f1eb54d3dbe7d084417df2aa68f36
JJWheel <josiah.wheeler@outlook.com>
2026-09-15 13:03:56 -0500

    new files

---
+++ in-class challenges/10-20sum.py
@@ -0,0 +1,5 @@
+num = [10, 12, 14, 16, 18, 20]
+total = 0
+for numbers in num:
+    total = total + numbers
+print(total)
---
+++ in-class challenges/recursion/recursionexample1.py
@@ -0,0 +1,7 @@
+def function(n) :
+    if n==4:
+        return n
+    else:
+        return 2*function(n+1)
+
+print(function(2))
